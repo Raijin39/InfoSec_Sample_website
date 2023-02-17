7d58d8f875f41f6d6a3e3b98570cf2a587e61225
jerichogipoleo <jerichogipoleo@gmail.com>
2023-02-17 11:09:46 +0800

    php update admindashboard 2/17/23

---
+++ includeadmin/navbaradmin.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-lg bg-info" id="navbar">
+        <div class="container">
+            <a class="navbar-brand fw-bold text-uppercase text-white" href="#">Information Security and Assurance</a>
+            <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
---
+++ includeadmin/offcanvasadmin.php
@@ -0,0 +1,27 @@
+<div class="offcanvas offcanvas-start sidebar-nav" tabindex="-1" id="offcanvasExample"
+        aria-labelledby="offcanvasExampleLabel">
+        <div class="offcanvas-body">
+            <div class="">
+                <ul class="nav nav-pills nav-fill flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link text-black" href="#">Messages</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-black" href="#">Comments</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-black" href="#">Posts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-black" href="#">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-black" href="#">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
