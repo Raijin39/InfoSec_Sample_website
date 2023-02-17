63211e5baacffa601d1d80d4d80ff4169e6077a5
Raijin39 <equiasangelo@gmail.com>
2023-02-17 11:32:15 +0800

    update on the home frontend 17/02/2023

--- include/contact.php
+++ include/contact.php
@@ -1,37 +1,67 @@
-<!--Contact section-->
-<section class="ContactUs" id="ContactUs">
-    <div class="max-width">
-        <h2 class="title">Contact Us</h2>
-        <div class="Contact-content">
-            <div class="text">Get In Touch Today</div>
-            <p>
+      <!--Contact section-->
+      <section class="Contact" id="Contact">
+        <div class="max-width">
+          <h2 class="title">Contact Us</h2>
+          <div class="Contact-content">
+            <div class="collumn left">
+              <div class="text">Get In Touch Today</div>
+              <p>
                 Putting up a website is really hard these days. But don't you
-                worry, we got you covered. All we need is some information about
+                worry, we got you covered. All we need some tnformauon about
                 your business and we'll get started right away. coded by Raijin
                 2022
-            </p>
-            <div class="icons">
+              </p>
+              <div class="icons">
                 <div class="row">
+                  <img src="images/address-book-solid.svg" />
                   <div class="info">
                     <div class="head">Company Name</div>
                     <div class="sub-title">IAM WEBDEV CORP.</div>
                   </div>
                 </div>
-
                 <div class="row">
+                  <img src="images/map-marked-alt-solid.svg" />
                   <div class="info">
                     <div class="head">Address</div>
                     <div class="sub-title">Sampaloc, Manila, Philippines</div>
                   </div>
                 </div>
-
                 <div class="row">
-                    <div class="info">
-                        <div class="head">Email Us</div>
-                        <div class="sub-title">equiasangelo@gmail.com</div>
-                    </div>
+                  <img src="images/envelope-open-text-solid.svg" />
+                  <div class="info">
+                    <div class="head">Email Us</div>
+                    <div class="sub-title">equiasangelo@gmail.com</div>
+                  </div>
                 </div>
+              </div>
             </div>
+            <div class="collumn right">
+              <div>Message Us</div>
+              <form action="#">
+                <div class="fields">
+                  <div class="field name">
+                    <input type="text" placeholder="Name" required />
+                  </div>
+                  <div class="field email">
+                    <input type="email" placeholder="Email" required />
+                  </div>
+                </div>
+                <div class="field">
+                  <input type="text" placeholder="Subject" required />
+                </div>
+                <div class="field textarea">
+                  <textarea
+                    cols="30"
+                    rows="10"
+                    placeholder="Message"
+                    required
+                  ></textarea>
+                </div>
+                <div class="button">
+                  <button type="submit">Send Message</button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-    </div>
-</section>
+      </section>
---
+++ indexb4login.php
@@ -0,0 +1,23 @@
+<?php include('include/header.php');?>
+
+<?php include('include/navbar.php');?>
+
+ <!-- Scroll Spy Main Content -->
+ <main
+      data-bs-spy="scroll"
+      data-bs-target="#navbar-example2"
+      data-bs-root-margin="0px 0px -40%"
+      data-bs-smooth-scroll="true"
+      class="scrollspy-example"
+      tabindex="0"
+    >
+        <?php include('include/home.php');?>
+        <?php include('include/about.php');?>
+        <?php include('include/services.php');?>
+        <?php include('include/skills.php');?>
+        <?php include('include/comment.php');?>
+        <?php include('include/contactlogin.php');?>
+        
+    </main>
+
+<?php include('include/footer.php');?>
