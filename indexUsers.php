03fb617b780e1274074f24266043fca00976b677
Marc Remolin <marcremolin.19@gmail.com>
2023-02-21 17:48:23 +0800

    Admin update 2/21/23

--- indexUsers.php
+++ indexUsers.php
@@ -1,6 +1,6 @@
 <?php $page_title = "Users";
 include('include/header.php');
-include('include/navbar.php');?>
+include('includeadmin/navbaradmin.php');?>
 <?php include('includeadmin/offcanvasadmin.php');?>
 <?php include('includeadmin/tableUsersAdmin.php');?>
 <?php include('include/footer.php');?>
