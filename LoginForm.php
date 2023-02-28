23863de9c269cb54c94095a6d38fddb2fa623eed
Raijin39 <equiasangelo@gmail.com>
2023-02-28 14:12:11 +0800

    login fixes

--- LoginForm.php
+++ LoginForm.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION["user"])) {
-   header("Location: icludeLogin/login.php");
+   header("Location: includeLogin/login.php");
 }
 ?>
 <!DOCTYPE html>
@@ -11,13 +11,13 @@ if (!isset($_SESSION["user"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="icludeLogin/style.css">
+    <link rel="stylesheet" href="includeLogin/style.css">
     <title>User Dashboard</title>
 </head>
 <body>
     <div class="container">
         <h1>Welcome to Dashboard</h1>
-        <a href="icludeLogin/logout.php" class="btn btn-warning">Logout</a>
+        <a href="includeLogin/logout.php" class="btn btn-warning">Logout</a>
     </div>
 </body>
 </html>
