cf25e44a65aceadb08378991bde0a6d9b4838077
Marc Remolin <marcremolin.19@gmail.com>
2023-03-07 15:47:12 +0800

    tablePost update 3/7/13

--- includeadmin/contentAdmin.php
+++ includeadmin/contentAdmin.php
@@ -2,10 +2,10 @@
 <!-- NAV starts -->
     <div class="nav flex-column nav-pills me-3 ps-5 pe-5 pt-1" id="v-pills-tab" role="tablist" aria-orientation="vertical">
         <ul class="list-unstyled">
-            <li><button class="nav-link active text-center" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button></li>
-            <li><button class="nav-link text-center" id="posts-tab" data-bs-toggle="pill" data-bs-target="#posts" type="button" role="tab" aria-controls="posts" aria-selected="false">Posts</button></li>
-            <li><button class="nav-link text-center" id="users-tab" data-bs-toggle="pill" data-bs-target="#users" type="button" role="tab" aria-controls="users" aria-selected="false">Users</button></li>
-            <a href="logout.php" class="btn btn-warning">Logout</a>
+            <li><button class="nav-link active text-center mb-1" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button></li>
+            <li><button class="nav-link text-center mb-1" id="posts-tab" data-bs-toggle="pill" data-bs-target="#posts" type="button" role="tab" aria-controls="posts" aria-selected="false">Posts</button></li>
+            <li><button class="nav-link text-center mb-1" id="users-tab" data-bs-toggle="pill" data-bs-target="#users" type="button" role="tab" aria-controls="users" aria-selected="false">Users</button></li>
+            <a href="logout.php" class="btn btn-outline-primary">Logout</a>
 
         </ul>
     </div>
