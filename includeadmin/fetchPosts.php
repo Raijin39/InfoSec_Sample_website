89c1e7a971b750df17b76bfdd1ee4de9412382fc
Marc Remolin <marcremolin.19@gmail.com>
2023-03-07 17:18:04 +0800

    Revert "Update fetchPosts.php"
    This reverts commit 4b8c8f8e4aaf976e7b88c89ef543e0512c242e90.

--- includeadmin/fetchPosts.php
+++ includeadmin/fetchPosts.php
@@ -11,10 +11,7 @@
                 <td>
                     <div class='gap-2 d-md-flex justify-content-center align-items-center'> 
                         <button type='button' class='btn btn-primary'><a class='text-decoration-none text-white' href='postedit.php?updateid=".$fetch['id']."'>Edit</a></button>
-                        <form method='post' action='deletePost.php'>
-                            <input type='hidden' name='id' value=" .$fetch['id']. ">
-                            <button class='btn btn-outline-danger' type='submit' name='deletePost'>Delete</button> 
-                        </form>
+                        <button type='button' class='btn btn-outline-danger'>Delete</button>
                     </div>
                 </td>
             </tr>
